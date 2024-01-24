bef2c36c9c9ed90c2579181da9d8d6f2e67bf839
Michael Gregory <mr.m.gregory@outlook.com>
2024-01-24 14:59:18 +0000

    add remove button

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,44 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState("");
   const [items, setItems] = useState([]);
 
   const addItem = () => {
-    if (inputValue.trim() !=='') {
+    if (inputValue.trim() !== "") {
       setItems([...items, inputValue]);
-      setInputValue('');
+      setInputValue("");
     }
-  }
+  };
+
+  const removeItem = (index) => {
+    const updatedItems = [...items];
+    updatedItems.splice(index, 1);
+    setItems(updatedItems);
+  };
+
   return (
     <>
-    <div>
-      <input 
-      type="text" 
-      value={inputValue}
-      placeholder='Type here'
-      onChange={e => setInputValue(e.target.value)}
-      />
-      <button onClick={addItem}>+</button>
-      <ul>
-        {items.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
-    </div>
+      <div>
+        <input
+          type="text"
+          value={inputValue}
+          placeholder="Type here"
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button onClick={addItem}>+</button>
+        <ul>
+          {items.map((item, index) => (
+            <li key={index}>
+              {item}
+              <button onClick={() => removeItem(index)}>Remove</button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
