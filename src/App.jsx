fab5ad6c9b13965d81cdc0ab7c6b66ec49ed7032
Michael Gregory <mr.m.gregory@outlook.com>
2024-01-24 14:01:16 +0000

    Adding the task to add lists to the page

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,31 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [inputValue, setInputValue] = useState('');
+  const [items, setItems] = useState([]);
 
+  const addItem = () => {
+    if (inputValue.trim() !=='') {
+      setItems([...items, inputValue]);
+      setInputValue('');
+    }
+  }
   return (
     <>
-  
+    <div>
+      <input 
+      type="text" 
+      value={inputValue}
+      placeholder='Type here'
+      onChange={e => setInputValue(e.target.value)}
+      />
+      <button onClick={addItem}>+</button>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
     </>
   )
 }
